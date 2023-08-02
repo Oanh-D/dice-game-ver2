f5dcbc85d2812fc1355f2fd5acc67d56d5d43fb4
Oanh-D <128280161+Oanh-D@users.noreply.github.com>
2023-08-02 16:10:35 -0700

    Add files via upload

---
+++ dicee.js
@@ -0,0 +1,27 @@
+
+//player 1
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+
+var randomImage1 = "/images/dice" + randomNumber1 + ".png";
+
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImage1);
+//player 2
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+
+var randomImage2 = "/images/dice" + randomNumber2+ ".png";
+
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImage2);
+
+//change h1
+if (randomImage1 > randomImage2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 wins!";
+}
+else if (randomImage1 < randomImage2) {
+    document.querySelector("h1").innerHTML = "Player 2 wins! 🚩";
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
+
